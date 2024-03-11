4a482732b7aa7a39c16e5f22d5abf48add67bfdb
gerganakaloferova <gergana.kaloferova006@gmail.com>
2024-03-11 18:17:39 +0200

    test_dbcontext

---
+++ Data/RecipeCatalogDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Data.Models;
+
+namespace Data
+{
+    public class RecipeCatalogDbContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<RecipeType> RecipeTypes { get; set; }
+        public DbSet<Product_Recipe> Products_Recipes { get; set; }
+
+        public RecipeCatalogDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=.;Database=RecipeCatalogDB;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
+
