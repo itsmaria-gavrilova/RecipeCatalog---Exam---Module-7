e4dc3d7acafb452ba9e1d300f7a7f0ab82a0d22c
User <User@DESKTOP-HLUADTU>
2024-03-11 16:57:18 +0200

    Add project files.

---
+++ RecipeCatalog - Exam - Module 7/Form1.cs
@@ -0,0 +1,10 @@
+namespace RecipeCatalog___Exam___Module_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
