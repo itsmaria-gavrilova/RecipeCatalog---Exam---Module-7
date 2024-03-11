bb8046bddab9d7b8de57d3d9027cb1dfa2ad93a6
gerganakaloferova <gergana.kaloferova006@gmail.com>
2024-03-11 18:11:01 +0200

    test

---
+++ Data/Models/Product_Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Product_Recipe
+    {
+        [Key]
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+
+        [Key]
+        [ForeignKey(nameof(Recipe))]
+        public int RecipeId { get; set; }
+
+        public Product Product { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
