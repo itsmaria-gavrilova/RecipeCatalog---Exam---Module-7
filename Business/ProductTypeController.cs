d0b800f74d030b9bf7e4dc22eedbaf4327720d20
gerganakaloferova <gergana.kaloferova006@gmail.com>
2024-03-12 20:11:40 +0200

    test

---
+++ Business/ProductTypeController.cs
@@ -0,0 +1,61 @@
+﻿using Data;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProductTypeController
+    {
+        private RecipeCatalogDbContext dbContext;
+        public List<ProductType> GetAll()
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                return dbContext.ProductTypes.ToList();
+            }
+        }
+        public ProductType Get(int id)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                return dbContext.ProductTypes.Find(id);
+            }
+        }
+        public void Add(ProductType productType)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                dbContext.ProductTypes.Add(productType);
+                dbContext.SaveChanges();
+            }
+        }
+        public void Update(ProductType productType)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                var item = dbContext.ProductTypes.Find(productType.Id);
+                if (item != null)
+                {
+                    dbContext.Entry(item).CurrentValues.SetValues(productType);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                var productType = dbContext.ProductTypes.Find(id);
+                if (productType != null)
+                {
+                    dbContext.ProductTypes.Remove(productType);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- Business/RecipeController.cs
+++ Business/RecipeController.cs
@@ -31,6 +31,33 @@ namespace Business
                 }
             }
         }
+        public Recipe GetByName(string name)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                return dbContext.Recipes.Find(name);
+            }
+        }
+        public void Add(Recipe recipe)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                dbContext.Recipes.Add(recipe);
+                dbContext.SaveChanges();
+            }
+        }
+        public void Update(Recipe recipe)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                var item = dbContext.Recipes.Find(recipe.Name);
+                if (item != null)
+                {
+                    dbContext.Entry(item).CurrentValues.SetValues(recipe);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
         public void SortByCalories()
         {
             using(dbContext=new RecipeCatalogDbContext())
@@ -47,6 +74,35 @@ namespace Business
                 return dbContext.Recipes.Where(x => x.TypeId == id).ToList();
             }
         }
-
+        public List<Recipe> Top5ByRating()
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                List<Recipe> sortedRecipes = dbContext.Recipes.OrderByDescending(x => x.Rating).ToList();
+                List<Recipe> top5ByRating = new List<Recipe>();
+                for (int i = 0; i < 5; i++)
+                {
+                    top5ByRating.Add(sortedRecipes[i]);
+                }
+                return top5ByRating;
+            }
+        }
+        public double CalculatePrice(Recipe recipe)
+        {
+            using (dbContext = new RecipeCatalogDbContext())
+            {
+                double totalPrice = 0;
+                List<Product_Recipe> recipeProducts = dbContext.Products_Recipes.Where(x => x.RecipeId == recipe.Id).ToList();
+                for (int i = 0; i < recipeProducts.Count; i++)
+                {
+                    List<Product> products = dbContext.Products.Where(x => x.Id == recipeProducts[i].ProductId).ToList();
+                    for (int j = 0; j < products.Count; j++)
+                    {
+                        totalPrice += products[j].Price;
+                    }
+                }
+                return totalPrice;
+            }
+        }
     }
 }
